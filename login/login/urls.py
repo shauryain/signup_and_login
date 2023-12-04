d48ce5510edda617f4860054237b3d18e2141e97
shauryain <147681669+shauryain@users.noreply.github.com>
2023-12-04 17:14:40 +0530

    signup
    signup holds the key things related to the use of detail and authentication

--- login/login/urls.py
+++ login/login/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from login_page.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",include("login_page.urls"))
 ]
