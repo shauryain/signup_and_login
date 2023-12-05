94faa81c54262d40b4857ba0aa4ecc2bc8e9f0a0
shauryain <147681669+shauryain@users.noreply.github.com>
2023-12-05 11:14:18 +0530

    signup
    page are pixed

--- login/login_page/views.py
+++ login/login_page/views.py
@@ -23,43 +23,43 @@ def signup(request):
         return render(request, 'signup.html')
     
 
-def login(request):
-    if request.method == 'POST':
-        uname=request.POST.get('uname')
-        pswd=request.POST.get('upassword')
-        user= authenticate(request,username=uname,password=pswd)
 
-        if user is not None:
-            login(request,user)
-            return redirect("/")
-        else:
-            d = {"msg": "Invalid username or password"}
-            return render(request,"login.html", d)
-        
-    else:
 
-        return render(request, "login.html")
-
-# from django.contrib.auth import authenticate, login
-# from django.contrib import messages
-# from django.shortcuts import render, redirect
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         uname = request.POST.get('uname')
-#         pswd = request.POST.get('upassword')
-
-#         user = authenticate(request, username=uname, password=pswd)
-
-#         if user is not None:
-#             login(request, user)
-#             return redirect("/")
-#         else:
-#             # Use add_message() to add an error message
-#             messages.add_message(request, messages.ERROR, "Invalid username or password")
-#             return render(request, "login.html")
-#     else:
-#         return render(request, 'login.html')
 
 def error(request):
     return redirect(request,"error.html")
+
+# def login(request):
+#     if request.method == 'POST':
+#         uname=request.POST.get('uname')
+#         pswd=request.POST.get('upassword')
+#         user= authenticate(request,username=uname,password=pswd)
+
+#         if user is not None:
+#             login(request,user)
+#             return redirect("/")
+#         else:
+#             d = {"msg": "Invalid username or password"}
+#             return render(request,"login.html", d)
+
+#     else:
+
+#         return render(request, "login.html")
+
+from django.contrib.auth import login as auth_login
+
+def login(request):
+    if request.method == 'POST':
+        uname = request.POST.get('uname')
+        pswd = request.POST.get('upassword')
+        user = authenticate(request, username=uname, password=pswd)
+
+        if user is not None:
+            auth_login(request, user)  # Use the aliased function
+            return redirect("/")
+        else:
+            d = {"msg": "Invalid username or password"}
+            return render(request, "login.html", d)
+    else:
+        return render(request, "login.html")
+
